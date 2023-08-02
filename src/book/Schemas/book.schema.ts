5832228894b2cd777fbbed8f4b52da74cef24ac2
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-08-02 19:10:23 +0200

    book shcema

---
+++ src/book/Schemas/book.schema.ts
@@ -0,0 +1,21 @@
+import { Schema } from "@nestjs/mongoose";
+
+export enum Category {
+    ADVENTURE = 'Adventure',
+    CLASSICS = 'Classics',
+    CRIME = 'Crime',
+    FANTASY = 'Fantasy'
+}
+
+
+@Schema({
+    timestamps: true
+})
+export class Book {
+    title: string;
+    description: string;
+    author: string;
+    price: number;
+    category :  Category
+
+}
