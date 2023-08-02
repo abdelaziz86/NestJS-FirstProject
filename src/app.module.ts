9421d2ce91bd8f416dfd7c3dc09c0477a31169e8
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-08-02 19:06:28 +0200

    db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [BookModule],
+  imports: [
+    ConfigModule.forRoot({ // ============ for env
+      envFilePath: '.env',
+      isGlobal : true
+    }),
+    MongooseModule.forRoot(process.env.DB_URI, {
+      dbName: process.env.DB_NAME 
+    }), // ============ for mongo
+    BookModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
