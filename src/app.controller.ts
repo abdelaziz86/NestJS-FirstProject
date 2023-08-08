45aa89a4e302181ff6d13af4daa87dff5dfe4712
abdelaziz86 <62603535+abdelaziz86@users.noreply.github.com>
2023-08-08 13:24:00 +0200

    SSE 2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Sse } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Observable } from 'rxjs';
+import { Observable, interval, map } from 'rxjs';
 
 interface MessageEvent{
   data: string | object; 
@@ -17,7 +17,11 @@ export class AppController {
 
   @Sse('event')
   sendEvent() : Observable<MessageEvent> {
-    return; 
+    return interval(1000).pipe(
+      map((num: number) => ({
+        data : 'hello' + num,
+      }))
+    ); 
   }
 
 }
